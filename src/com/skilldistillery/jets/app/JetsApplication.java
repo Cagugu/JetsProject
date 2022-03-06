54089497e27eca5e6d77845c4100c8103acaefd9
Cecelia Guerrero <whit3fenc3@gmail.com>
2022-03-05 19:33:43 -0600

    Removed repeat loop in menu

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -6,7 +6,7 @@ import com.skilldistillery.jets.entities.AirField;
 
 public class JetsApplication {
 
-	Scanner kb; 
+	Scanner kb;
 	private AirField af;
 
 	public static void main(String[] args) {
@@ -15,19 +15,19 @@ public class JetsApplication {
 		menuInstance.launch();
 
 	}
-	private void launch(){
+
+	private void launch() {
 		af = new AirField();
 		menu();
 	}
 
 	private void menu() {
 		kb = new Scanner(System.in);
-		int userChoice = 0; 
+		int userChoice = 0;
 
+		while (userChoice != 9) {
 
-		while(userChoice<10) {
-			
-		
+			System.out.println();
 			System.out.println("Main Menu:");
 			System.out.println("Please enter 9 to exit menu.");
 			System.out.println("Enter 1 to: List the fleet: ");
@@ -38,42 +38,41 @@ public class JetsApplication {
 			System.out.println("Enter 6 to: Call in all available supply drops: ");
 			System.out.println("Enter 7 to: Add a new aircraft to the fleet: ");
 			System.out.println("Enter 8 to: Remove aircraft from fleet: ");
-			
+
 			userChoice = kb.nextInt();
-		
-		if(userChoice <9) {
-		switch (userChoice) {
-		case 1:
-			af.listFleet();
-			break;
-		case 2:
-			af.flyAllJets();
-			break;
-		case 3:
-			af.getFastestJet();
-			break;
-		case 4:
-			af.getLongestRange();	
-			break;
-		case 5:
-			af.dropAllExplorers();
-			break;
-		case 6: 
-			af.dropResupply();
-			break;
-		case 7: 
-			af.buildYourOwn();
-			break;
-		case 8:
-			af.removeJetFromFleet();
-		}
-	}
-		else {
+
+				switch (userChoice) {
+				case 1:
+					af.listFleet();
+					break;
+				case 2:
+					af.flyAllJets();
+					break;
+				case 3:
+					af.getFastestJet();
+					break;
+				case 4:
+					af.getLongestRange();
+					break;
+				case 5:
+					af.dropAllExplorers();
+					break;
+				case 6:
+					af.dropResupply();
+					break;
+				case 7:
+					af.buildYourOwn();
+					break;
+				case 8:
+					af.removeJetFromFleet();
+					break;
+
+				}
+		if (userChoice == 9) {
 			System.out.println("You have chosen to quit. See you next time!");
+			break;
 		}
 		}
 	}
-	
-	
 
 }
